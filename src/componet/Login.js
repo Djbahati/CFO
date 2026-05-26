2ea004ff65a2856dbf1602a61249557c1e2761a0
DJBAHATI <peterbahati0455033@gmail.com>
2026-05-26 13:39:51 +0200

    Update frontend dependencies

--- src/componet/Login.js
+++ src/componet/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
     return (
         <div>
             <h1>Login</h1>
-            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+             <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
             <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
             <button onClick={handleLogin}>Login</button>
         </div>
